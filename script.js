4694b38497785201094398de8e4503f9a92bcf65
Mateus <74675004+clmateus@users.noreply.github.com>
2021-10-03 15:21:59 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+// Arrays para classificar cada array dos métodos de data 
 dia = new Array("Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado");
 mes = new Array("Janeiro", "Fevereiro", "Março", "Abril", "Maio", "Junho", "Julho", "Agosto", "Setembro", "Outubro", "Novembro", "Dezembro");
 
+// É necessário a expecificação que será usada a propriedade Date
 agora = new Date;
 
+// Essa variável será responsável por selecionar o elemento HTML que sofrerá alterações
 let h1 = document.getElementById("h1");
+
+// Essa linha é responsável por modificar o texto do HTML.
 h1.innerHTML = agora.getDate() + " de " + mes[agora.getMonth()] + " de " + agora.getFullYear();
